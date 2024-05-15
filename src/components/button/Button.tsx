71f2ea5ff4d443a4374640eda3ee5816872e63e7
Afeef Zaman <azaman@globalrwc.com>
2024-05-15 10:59:34 -0700

    Use styled components for button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,5 +1,5 @@
 import React, { ButtonHTMLAttributes, forwardRef, ReactNode } from "react";
-import classes from "./Button.module.css";
+import styled from "styled-components";
 
 export interface ButtonProps
   extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, "title"> {
@@ -10,36 +10,136 @@ export interface ButtonProps
   children: ReactNode;
 }
 
+const StyledButton = styled.button<ButtonProps>`
+  display: inline-flex;
+  padding: 0.5rem 1rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.75rem;
+
+  border-radius: 0.25rem;
+
+  /* shadow/sm */
+
+  box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.05);
+
+  /* Body/Body bold */
+  font-family: Mulish;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 150%;
+  /* 1.5rem */
+  letter-spacing: 0.00119rem;
+`;
+
+const PrimaryStyledButton = styled(StyledButton)`
+  background: var(--primary-200, #177ba6);
+  color: var(--system-50, #fff);
+
+  &:hover {
+    font-weight: 700;
+    background: var(--primary-300, #115873);
+    color: var(--system-50, #fff);
+  }
+
+  &:focus {
+    border: 2px solid var(--primary-200, #177ba6);
+    background: var(--primary-50, #2aace2);
+    color: var(--system-50, #fff);
+  }
+
+  &:disabled {
+    background: var(--neutral-50, #f9fafb);
+    color: var(--neutral-50, #9ca3af);
+  }
+`;
+
+const SecondaryStyledButton = styled(StyledButton)`
+  height: 2.625rem;
+  flex-shrink: 0;
+  border: 1px solid var(--neutral-50, #9ca3af);
+  background: var(--system-50, #fff);
+  color: var(--primary-200, #177ba6);
+
+  &:hover {
+    border: 1px solid var(--neutral-100, #1c99ce);
+    color: var(--primary-200, #177ba6);
+  }
+
+  &:focus {
+    border: 2px solid var(--primary-200, #177ba6);
+  }
+
+  &:disabled {
+    background: var(--neutral-50, #f9fafb);
+    color: var(--neutral-200, #9ca3af);
+  }
+`;
+
+const NegativeStyledButton = styled(StyledButton)`
+  background: var(--negative-100, #ef4444);
+  color: var(--system-50, #fff);
+
+  &:hover {
+    background: var(--negative-200, #7f1d1d);
+  }
+
+  &:focus {
+    border: 2px solid var(--negative-200, #7f1d1d);
+    background: var(--negative-50, #f87171);
+  }
+
+  &:disabled {
+    background: var(--neutral-50, #f9fafb);
+    color: var(--neutral-200, #9ca3af);
+  }
+`;
+
+const PositiveStyledButton = styled(StyledButton)`
+  background: var(--positive-100, #059669);
+  color: var(--system-50, #fff);
+
+  &:hover {
+    background: var(--positive-200, #065f46);
+  }
+
+  &:focus {
+    border: 2px solid var(--positive-200, #065f46);
+    background: var(--positive-50, #34d399);
+    color: var(--positive-200, #065f46);
+  }
+
+  &:disabled {
+    background: var(--neutral-50, #f9fafb);
+    color: var(--neutral-200, #9ca3af);
+  }
+`;
+
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   (
     {
       loading = false,
       disabled = false,
-      classNames = "",
       variant = "primary",
       children,
       ...rest
     }: ButtonProps,
     ref: React.Ref<HTMLButtonElement>,
   ) => {
-    const buttonClasses = {
-      primary: "primary-button",
-      secondary: "secondary-button",
-      negative: "negative-button",
-      positive: "positive-button",
+    const StyledComponent = {
+      primary: PrimaryStyledButton,
+      secondary: SecondaryStyledButton,
+      negative: NegativeStyledButton,
+      positive: PositiveStyledButton,
     };
 
-    const className = `${classes.button} ${classes[buttonClasses[variant]] || classes[buttonClasses.primary]} ${classNames} ${disabled ? "disabled" : ""} `;
+    const ButtonComponent = StyledComponent[variant];
 
     return (
-      <button
-        ref={ref}
-        className={className}
-        disabled={loading || disabled}
-        {...rest}
-      >
+      <ButtonComponent ref={ref} disabled={loading || disabled} {...rest}>
         {children}
-      </button>
+      </ButtonComponent>
     );
   },
 );
